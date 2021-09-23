44a55cf23093470c01e34c69e3892819fffa01e3
adamsantilli2 <asantil1@terpmail.umd.edu>
2021-09-23 14:54:01 -0400

    file setup and initial steps in youtube video

---
+++ client/lab_4/script.js
@@ -0,0 +1,8 @@
+let slidePosition = 0;
+const slides = document.getElementsByClassName('carousel_item');
+const totalSlides = slides.length;
+
+document.getElementById('carousel_button--next')
+    .addEventListener("click", function() {
+        console.log('hello=-------')
+    })
