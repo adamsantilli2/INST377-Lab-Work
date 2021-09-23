abd06d450ec55fd504c5a99cab23162ce5aacafe
adamsantilli2 <asantil1@terpmail.umd.edu>
2021-09-23 17:34:06 -0400

    added button actions

--- client/lab_4/script.js
+++ client/lab_4/script.js
@@ -4,5 +4,36 @@ const totalSlides = slides.length;
 
 document.getElementById('carousel_button--next')
     .addEventListener("click", function() {
-        console.log('hello=-------')
-    })
+        moveToNextSlide();
+    });
+document.
+    getElementById('carousel_button--prev')
+    .addEventListener("click", function() {
+        moveToPrevSlide();
+    });
+
+    function updateSlidePosition() {
+        for (let slide of slides) {
+            slide.classList.remove('carousel_item--visible');
+            slide.classList.add('carousel_item--hidden');
+        }
+    slides[slidePosition].classList.add('carousel_item--visible');
+    }
+
+    function moveToNextSlide() {
+        if (slidePosition === totalSlides -1) {
+            slidePosition = 0;
+        } else {
+            slidePosition++;
+        }
+        updateSlidePosition();
+    }
+
+    function moveToPrevSlide() {
+        if (slidePosition === 0) {
+            slidePosition = totalSlides -1;
+        } else {
+            slidePosition--;
+        }
+        updateSlidePosition();
+    }
